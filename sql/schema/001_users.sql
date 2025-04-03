dc4285b7994b4d7466cc50afaa6b6fca7cda4cb3
tshjustin <j.tann2305@gmail.com>
2025-04-03 15:59:28 +0800

    Updated Proj

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,20 +1,10 @@
--- Migrations are ordered - and they allow for managing changes to DB over time 
--- think of it as version control for DB schemas 
--- goose is a tool to handle migrations, similar to albemic python package 
--- Below up => up migration , below down => down migration (downgrade)
-
-
 -- +goose Up
-
 CREATE TABLE users (
     id UUID PRIMARY KEY,
-    created_at TIMESTAMP NOT NULL, 
+    created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     name TEXT NOT NULL
 );
 
--- goose postgres postgresql:... up | schemas > Tables > ... 
-
--- +goose Down 
-
+-- +goose Down
 DROP TABLE users;
